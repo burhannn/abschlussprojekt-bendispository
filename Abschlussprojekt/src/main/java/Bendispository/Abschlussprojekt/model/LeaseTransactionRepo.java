0438174d71531f5ab11d4df7e7add89c419c76e3
marcmachtdenanfang <marc.anfang@outlook.com>
2019-02-11 14:47:23 +0100

    LeaseTransaction and Repo - Added

---
+++ Abschlussprojekt/src/main/java/Bendispository/Abschlussprojekt/model/LeaseTransactionRepo.java
@@ -0,0 +1,6 @@
+package Bendispository.Abschlussprojekt.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface LeaseTransactionRepo extends CrudRepository<LeaseTransaction, Long> {
+}
