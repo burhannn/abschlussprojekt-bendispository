9febe8d58f80d82f47ccb1ef0151eb3a0ff97808
nifi611 <44160192+nifi611@users.noreply.github.com>
2019-02-12 11:25:03 +0100

    Controller changed

--- Abschlussprojekt/src/main/java/Bendispository/Abschlussprojekt/controller/PaymentController.java
+++ Abschlussprojekt/src/main/java/Bendispository/Abschlussprojekt/controller/PaymentController.java
@@ -3,6 +3,7 @@ package Bendispository.Abschlussprojekt.controller;
 import Bendispository.Abschlussprojekt.repo.PaymentTransactionRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PaymentController {
@@ -10,6 +11,11 @@ public class PaymentController {
     @Autowired
     PaymentTransactionRepo paymentTransactionRepo;
 
+    @GetMapping(path = "/profile/PaymentTransaction"){
+        public String concludeTransaction(Model model){
+            return "/";
+        }
+    }
 
 
 }
