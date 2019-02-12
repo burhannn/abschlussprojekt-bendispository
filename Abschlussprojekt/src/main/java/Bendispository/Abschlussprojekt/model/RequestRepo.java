0cf72f00ffe86845a588e2c71e72a311f9baab91
marcmachtdenanfang <marc.anfang@outlook.com>
2019-02-12 10:03:59 +0100

    Request System - Added

---
+++ Abschlussprojekt/src/main/java/Bendispository/Abschlussprojekt/model/RequestRepo.java
@@ -0,0 +1,7 @@
+package Bendispository.Abschlussprojekt.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RequestRepo extends CrudRepository<Request, Long>{
+
+}
