98d9645dfeab1553099bb4e67952d4a694d77bb1
burhannn <burhanbucak@hotmail.de>
2019-02-14 14:38:23 +0100

    tracking logged in user and show username

---
+++ Abschlussprojekt/src/main/java/Bendispository/Abschlussprojekt/Controller/LoginController.java
@@ -0,0 +1,25 @@
+package Bendispository.Abschlussprojekt.controller;
+
+import Bendispository.Abschlussprojekt.Service.MyUserPrincipal;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+
+@Controller
+public class LoginController {
+    @RequestMapping(value="/username", method = RequestMethod.GET)
+    @ResponseBody
+    public String currentUsername(HttpServletRequest request ) {
+        Principal principal = request.getUserPrincipal();
+
+        return principal.getName();
+
+    }
+}
--- Abschlussprojekt/src/main/java/Bendispository/Abschlussprojekt/model/Person.java
+++ Abschlussprojekt/src/main/java/Bendispository/Abschlussprojekt/model/Person.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Data
 public class Person {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String lastName;
