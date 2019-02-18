cde20e922feafa1b5fcd91580d0c8e1059c37d52
Jannik K <Jakos103@hhu.de>
2019-02-18 15:27:08 +0100

    registration fixed

--- Abschlussprojekt/src/main/java/Bendispository/Abschlussprojekt/security/SecurityConfig.java
+++ Abschlussprojekt/src/main/java/Bendispository/Abschlussprojekt/security/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 @Configuration
@@ -33,7 +32,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.and()
 				.logout().permitAll()
 				.logoutSuccessUrl("/loggedOut");
-
     }
 
     @Override
