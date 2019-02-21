963c6c451595719e6e965160020d2eb091d9a9ca
clara <clara.maassen@gmx.de>
2019-02-21 13:18:12 +0100

    dont touch

--- Abschlussprojekt/src/main/java/Bendispository/Abschlussprojekt/service/AuthenticationService.java
+++ Abschlussprojekt/src/main/java/Bendispository/Abschlussprojekt/service/AuthenticationService.java
@@ -16,7 +16,7 @@ public class AuthenticationService {
         this.personsRepo = personsRepo;
     }
 
-    /////DONT TOUCH THIS FUNCTION
+    /////DONT TOUCH THIS FUNCTION!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     public Person getCurrentUser(){
         MyUserPrincipal userDetails = (MyUserPrincipal)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return personsRepo.findByUsername(userDetails.getUsername());
